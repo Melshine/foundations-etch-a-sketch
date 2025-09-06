348fc0801a2d34a6d6cdb15d44ab252d55342aa1
Melshine <11990946+Melshine@users.noreply.github.com>
2025-09-07 00:12:41 +0200

    feat: add random coloring and opacity when hovering

--- script.js
+++ script.js
@@ -11,12 +11,21 @@ function destroyGrid() {
 
 function createGrid(size = SIZE) {
 
-    function hoverEffect(event) {
-        const cell = event.target;
-        cell.classList.add('black');
+    function hoverEffect() {
+        if (this.style.backgroundColor === '') {
+            const r = Math.floor(Math.random() * 256);
+            const g = Math.floor(Math.random() * 256);
+            const b = Math.floor(Math.random() * 256);
+            this.style.backgroundColor = `rgb(${r},${g},${b})`;
+            this.style.opacity = .1;
+        } else {
+            this.style.opacity = Math.min(1, +this.style.opacity + .1);
+        }
     }
 
+    
     const grid = document.querySelector("#grid");
+    grid.style.width = `${GRID_WIDTH_PX}px`;
     for (let i = 0; i < size ** 2; i++) {
         const CELL_WIDTH = Math.floor(GRID_WIDTH_PX / size);
         const cell = document.createElement('div');
@@ -25,18 +34,16 @@ function createGrid(size = SIZE) {
         cell.addEventListener('mouseenter', hoverEffect);
         grid.appendChild(cell);
     }
-
-    grid.style.width = `${GRID_WIDTH_PX}px`;
-
 }
 
 createGrid();
 
 const button = document.querySelector('button');
-button.addEventListener('click', event => {
-    const size = +prompt('Give a number');
-    if (size > 0 && size <= 100) {
+button.addEventListener('click', _ => {
+    const size = +prompt('Give a number between 0 and 100');
+    if (typeof size == 'number' && size > 0 && size <= 100) {
+        sizeInt = Math.floor(size);
         destroyGrid();
-        createGrid(size);
+        createGrid(sizeInt);
     }
 })
