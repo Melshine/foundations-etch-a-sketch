975670001025024159608c7c8743cdc7ae820c7c
Melshine <11990946+Melshine@users.noreply.github.com>
2025-09-06 19:32:57 +0200

    Initialize project and create a grid

---
+++ script.js
@@ -0,0 +1,12 @@
+const GRID_WITDH = 16;
+const CELL_WIDTH = 30;
+const grid = document.querySelector("#grid");
+
+for (let i = 0; i < GRID_WITDH ** 2; i++) {
+    const cell = document.createElement('div');
+    cell.style.width = `${CELL_WIDTH}px`;
+    cell.style.height = `${CELL_WIDTH}px`;
+    grid.appendChild(cell);
+}
+
+grid.style.width = `${GRID_WITDH * CELL_WIDTH}px`;
