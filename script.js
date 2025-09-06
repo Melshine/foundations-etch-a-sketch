695aa9d72770c04b7cd5bc188b6fd99d42cbcdce
Melshine <11990946+Melshine@users.noreply.github.com>
2025-09-06 20:29:09 +0200

    feat: add button to update grid size in number of cells

--- script.js
+++ script.js
@@ -1,17 +1,42 @@
-const GRID_WITDH = 16;
-const CELL_WIDTH = 30;
-const grid = document.querySelector("#grid");
+const GRID_WIDTH_PX = 600;
+const SIZE = 16;
 
-for (let i = 0; i < GRID_WITDH ** 2; i++) {
-    const cell = document.createElement('div');
-    cell.style.width = `${CELL_WIDTH}px`;
-    cell.style.height = `${CELL_WIDTH}px`;
-    grid.appendChild(cell);
+function destroyGrid() {
+    const grid = document.querySelector("#grid");
+    const newGrid = document.createElement('div');
+    newGrid.id = 'grid';
+    document.body.removeChild(grid);
+    document.body.appendChild(newGrid);
 }
 
-grid.style.width = `${GRID_WITDH * CELL_WIDTH}px`;
+function createGrid(size = SIZE) {
 
-grid.addEventListener('mouseover', event => {
-    const cell = event.target;
-    cell.classList.add('black');
+    function hoverEffect(event) {
+        const cell = event.target;
+        cell.classList.add('black');
+    }
+
+    const grid = document.querySelector("#grid");
+    for (let i = 0; i < size ** 2; i++) {
+        const CELL_WIDTH = Math.floor(GRID_WIDTH_PX / size);
+        const cell = document.createElement('div');
+        cell.style.width = `${CELL_WIDTH}px`;
+        cell.style.height = `${CELL_WIDTH}px`;
+        cell.addEventListener('mouseenter', hoverEffect);
+        grid.appendChild(cell);
+    }
+
+    grid.style.width = `${GRID_WIDTH_PX}px`;
+
+}
+
+createGrid();
+
+const button = document.querySelector('button');
+button.addEventListener('click', event => {
+    const size = +prompt('Give a number');
+    if (size > 0 && size <= 100) {
+        destroyGrid();
+        createGrid(size);
+    }
 })
